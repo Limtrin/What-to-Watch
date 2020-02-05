f8b855b32ccdbbe3c5a9601c402fa9e1eb615916
Limtrin <limonad13@gmail.com>
2020-02-05 10:57:38 +0400

    Личный проект: начинаем программировать

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  FILM_NAME: `The Grand Budapest Hotel`,
+  FILM_GENRE: `Drama`,
+  FILM_YEAR: `2014`
+};
+
+ReactDOM.render(
+    <App
+      filmName={Settings.FILM_NAME}
+      filmGenre={Settings.FILM_GENRE}
+      filmYear={Settings.FILM_YEAR}
+    />,
+    document.querySelector(`#root`)
+);
